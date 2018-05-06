4a773949fbb7fc0f0b3eed1f1a82832815c0a59a
Victor Cardona <victorcardona@Victors-MacBook-Air.local>
2018-05-06 18:31:47 -0500

    Service Categories and Category

---
+++ src/main/java/guru/springfamework/api/v1/model/CatorgoryListDTO.java
@@ -0,0 +1,15 @@
+package guru.springfamework.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class CatorgoryListDTO {
+
+    List<CategoryDTO> categories;
+
+}
+
